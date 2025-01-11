247099000fe81df2e65c552f60fe3915e792c588
bang0711 <bang07112004@gmail.com>
2025-01-11 15:51:34 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    typedRoutes: true,
-  },
-  images: {
-    remotePatterns: [{
-      hostname: '**',
-      pathname: '**',
-      protocol: 'https',
 
-    },{
-      hostname: '**',
-      pathname: '**',
-      protocol: 'http',
-      
-    }]
-  }
+  images: {
+    remotePatterns: [
+      {
+        hostname: "**",
+        pathname: "**",
+        protocol: "https",
+      },
+      {
+        hostname: "**",
+        pathname: "**",
+        protocol: "http",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
